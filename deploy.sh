2b7dfb960e80efcc2016dc5c33aec9608a86e94c
Asko Nõmm <asko.nomm@greenpowermonitor.com>
2021-02-26 23:47:20 +0000

    bump

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-bloggo && cp resources/root/* public/ && git add . && git commit -m "bump" && git push
+bloggo && \
+cp resources/root/* public/ && \
+git add . && \
+git commit -m "bump" \
+&& git push
